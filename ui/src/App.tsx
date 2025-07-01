8db0662b6221782cab658fb8f3123efeec9ea075
Steven <stevenxchung@gmail.com>
2025-07-01 14:09:54 -0400

    Switch to Sidebar

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,20 +4,22 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import { NavBar } from "./components/NavBar";
+import { Sidebar } from "./components/Sidebar";
 import { ChatPage } from "./pages/ChatPage";
 import { MediaPage } from "./pages/MediaPage";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Navigate to="/media" />} />
-          <Route path="/media" element={<MediaPage />} />
-          <Route path="/ai" element={<ChatPage />} />
-        </Routes>
+      <div className="flex h-screen bg-[#FDFEFF]">
+        <Sidebar />
+        <main className="flex flex-col flex-1 h-screen relative bg-[#FDFEFF] ">
+          <Routes>
+            <Route path="/" element={<Navigate to="/media" />} />
+            <Route path="/media" element={<MediaPage />} />
+            <Route path="/ai" element={<ChatPage />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
